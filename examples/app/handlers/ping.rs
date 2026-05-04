c92a642a184e722839d3bd64a89471c28a5cdf38
Duncan Crutchley <drd2k3.coding@tuta.io>
2026-05-04 12:26:02 +0100

    added missing licenses

--- examples/app/handlers/ping.rs
+++ examples/app/handlers/ping.rs
@@ -1,3 +1,24 @@
+// This file is part of core-net Rust crate containing useful reusable
+// networking utilities.
+//
+// Copyright (C) 2026 to present, Duncan Crutchley
+// Contact <15799155+dac1976@users.noreply.github.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License and GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// and GNU Lesser General Public License along with this program. If
+// not, see <http://www.gnu.org/licenses/>.
+
 use core_net::messaging::{
     dispatcher::{MessageContext, MessageDispatcherBuilder},
     message::Message,
